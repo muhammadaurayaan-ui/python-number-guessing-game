6e4e2527d96a94b721a63337e45bdac7daff1f02
muhammadaurayaan-ui <muhammadaurayaan@gmail.com>
2026-01-12 16:23:08 -0800

    Update guess_game.py

--- guess_game.py
+++ guess_game.py
@@ -1,11 +1,17 @@
-secret_number=52
+import random
+secret_number=random.randint(1,50)
 guess_count= 0
-guess_limit= 3 
+guess_limit= 3
 while guess_count < guess_limit:
-  guess=int(input("Enter your guess :"))
+  guess=int(input("Enter your guess ranging (1 to 50) :"))
   guess_count+=1
-  if guess==secret_number:
+  if guess>secret_number:
+      print("Too High!")
+  elif guess<secret_number:
+      print("Too Low!")
+  elif guess==secret_number:
     print("You Won!")
     break
 else:
   print("Sorry You failed!")
+  print(f"The correct Number was {secret_number}")
